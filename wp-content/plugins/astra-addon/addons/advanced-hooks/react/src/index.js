ec958696fea04e9dd8cab4485bb1a2f16a0c5ecf
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-06-12 06:55:20 +0530

    Modified .gitignore and cleared cache

--- wp-content/plugins/astra-addon/addons/advanced-hooks/react/src/index.js
+++
@@ -1,5 +0,0 @@
-import { registerPlugin } from '@wordpress/plugins';
-import customLayout from './settings';
-import { __ } from '@wordpress/i18n';
-
-registerPlugin( 'astra-custom-layout', { render: customLayout } );
